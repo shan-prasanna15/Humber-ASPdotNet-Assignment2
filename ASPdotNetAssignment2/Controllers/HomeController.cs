5e7d1a6b7bc5da94f039a5f01c610885a25ce6ae
Prasanna Shan <sporigins@gmail.com>
2020-09-15 19:30:07 -0400

    Sort, Filter and Group completed

--- ASPdotNetAssignment2/Controllers/HomeController.cs
+++ ASPdotNetAssignment2/Controllers/HomeController.cs
@@ -6,16 +6,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ASPdotNetAssignment2.Models;
+using ASPdotNetAssignment2.Models.SchoolViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASPdotNetAssignment2.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        
+        private readonly SchoolContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(SchoolContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -32,6 +35,19 @@ namespace ASPdotNetAssignment2.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public async Task<ActionResult> About()
+        {
+            IQueryable<EnrollmentDateGroup> data = from student in _context.Students
+                                                   group student by student.EnrollmentDate into dateGroup
+                                                   select new EnrollmentDateGroup()
+                                                   {
+                                                       EnrollmentDate = dateGroup.Key,
+                                                       StudentCount = dateGroup.Count()
+                                                   };
+
+            return View(await data.AsNoTracking().ToListAsync());
         }
     }
 }
